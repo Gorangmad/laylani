a4ac69d3cc7b8ab24b6cf4321bec7dd620b57a74
Gorang Madaan <ggorangmadaan@gmail.com>
2021-12-07 17:34:46 +0100

    'home'

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 3300
 
+//Assests
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
