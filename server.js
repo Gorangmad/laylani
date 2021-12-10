68e110c4d8d96b7adf0acd2b40b5e103b967d1d4
Gorang Madaan <ggorangmadaan@gmail.com>
2021-12-10 17:07:35 +0100

    ''

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require ('express')
 
 const app = express()
@@ -10,10 +12,49 @@ const expressLayout = require('express-ejs-layouts')
 
 const PORT = process.env.PORT || 3300
 
+const mongoose = require ('mongoose')
+
+const session = require ('express-session')
+
+const flash = require('express-flash')
+
+const MongoDbStore = require('connect-mongo');
+
+
+//Database connection
+
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log ('Database connected....');
+});
+
+
+//Session config
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({ mongoUrl: 'mongodb://localhost/pizza' }),
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60* 24} // 24 hours
+}))
+
+
+//Global middleware
+app.use((req, res, next) => {
+     res.locals.session = req.session
+     next()
+})
+
+
+app.use(flash())
+
 //Assests
 app.use(express.static('public'))
 
-
+app.use(express.json())
 
 app.use(expressLayout)
 
@@ -22,20 +63,9 @@ app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
+//Routes
+require('./routes/web')(app)
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
 
 app.listen(PORT , () =>{
     console.log('listening on port 3300')
